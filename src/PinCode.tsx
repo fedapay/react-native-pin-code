f3fe47625de532678e1e842fd543df7f2dfd1442
brexis2009@yahoo.fr <Koumondji Boris>
2022-01-21 19:47:28 +0100

    Fix

--- src/PinCode.tsx
+++ src/PinCode.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Vibration, Text, TouchableOpacity, Platform, ViewStyle } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import { PinCodeT, DEFAULT, PIN_KEY } from './types';
 import PinButton from './PinButton';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import PinCode from "./PinCode";
 import { PinCodeT, PIN_KEY } from "./types";
 
